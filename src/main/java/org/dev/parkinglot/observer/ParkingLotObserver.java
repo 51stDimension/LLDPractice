f0f5583e810dbbb01b06e3e1d99eb26170696d0c
51stDimension <siddharthmohanty0812@gmail.com>
2025-07-21 19:31:13 +0530

    main: fixing parking lot

--- src/main/java/org/dev/parkinglot/observer/ParkingLotObserver.java
+++ src/main/java/org/dev/parkinglot/observer/ParkingLotObserver.java
@@ -3,5 +3,5 @@ package org.dev.parkinglot.observer;
 import org.dev.parkinglot.models.EventType;
 
 public interface ParkingLotObserver {
-    void notify(EventType eventType, String parkingLotId)
+    void notify(EventType eventType, String parkingLotId);
 }
